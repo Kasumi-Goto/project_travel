53e0ce3bbcb72d343156ff220d45a5e8d54daf58
kasumi <lanius2020@hotmail.com>
2021-03-20 20:58:49 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <!-- 显示的是当前路由地址所对应的内容 -->
+        <router-view />
+    </div>
+</template>
+
+<script>
+// app.vue是一个单文件组件
+export default {
+    name: 'App'
+}
+</script>
+
+<style></style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <!-- 模板只能对外暴露一个根标签 -->
+    <div>hello world</div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style></style>
