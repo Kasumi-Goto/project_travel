d112f01b99d33efd9cb155b3516bd12e4ef0c656
kasumi <lanius2020@hotmail.com>
2021-03-21 19:46:58 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,17 @@
 <template>
     <!-- 模板只能对外暴露一个根标签 -->
-    <div>hello world</div>
+    <div>
+        <home-header></home-header>
+    </div>
 </template>
 
 <script>
+import HomeHeader from './components/header'
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+        HomeHeader
+    }
 }
 </script>
 
