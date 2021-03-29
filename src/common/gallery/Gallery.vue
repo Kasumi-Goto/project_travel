bc380ca8af94b59b03140b5452d2b256a0ce8999
kasumi <lanius2020@hotmail.com>
2021-03-29 15:04:15 +0800

    banner finish

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container" @click="handlegalleryClick">
+        <div class="wrapper">
+            <swiper :options="swiperOptions">
+                <swiper-slide v-for="(item, index) in imgs" :key="index">
+                    <img class="gallery-img" :src="item" />
+                </swiper-slide>
+                <div class="swiper-pagination" slot="pagination"></div>
+            </swiper>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CommonGallery',
+    props: {
+        imgs: {
+            type: Array,
+            default: function () {
+                return []
+            }
+        }
+    },
+    data: function () {
+        return {
+            swiperOptions: {
+                pagination: '.swiper-pagination',
+                paginationType: 'fraction',
+                observeParents: true,
+                observer: true
+            }
+        }
+    },
+    methods: {
+        handlegalleryClick: function () {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+    overflow inherit
+.container
+    // 居中显示图片
+    display flex
+    flex-direction column
+    justify-content center
+    z-index 99
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background #000
+    .wrapper
+        //撑出一个正方形
+        height 0
+        width 100%
+        padding-bottom 100%
+        .gallery-img
+            width 100%
+        .swiper-pagination
+            color #fff
+            bottom -1rem
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img
+                class="banner-img"
+                src="//img1.qunarzz.com/sight/p0/2005/a9/a9411416ff215fe2a3.water.jpg_600x330_feb1c240.jpg"
+            />
+            <div class="banner-info">
+                <div class="banner-title">上海迪士尼乐园</div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe702;</span>104
+                </div>
+            </div>
+        </div>
+        <common-gallery
+            :imgs="imgs"
+            v-show="showgallery"
+            @close="handlegalleryClose"
+        ></common-gallery>
+    </div>
+</template>
+
+<script>
+import CommonGallery from 'common/gallery/Gallery'
+export default {
+    name: 'DetailBanner',
+    data: function () {
+        return {
+            showgallery: false,
+            imgs: [
+                'http://img1.qunarzz.com/sight/p0/1905/37/37d8d7c0e8033ba3a3.img.jpg_r_800x800_fe56fe2d.jpg',
+                'http://img1.qunarzz.com/sight/p0/1905/89/896379d8f6c705e6a3.img.jpg_r_800x800_ec832d3a.jpg'
+            ]
+        }
+    },
+    components: {
+        CommonGallery
+    },
+    methods: {
+        handleBannerClick: function () {
+            this.showgallery = true
+        },
+        handlegalleryClose: function () {
+            this.showgallery = false
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+        width 100%
+    .banner-info
+        display flex
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        line-height .6rem
+        color #fff
+        background-image linear-gradient(to bottom,rgba(0,0,0,0),rgba(0,0,0,.8))
+        .banner-title
+            flex 1
+            font-size .32rem
+            padding 0 .2rem
+        .banner-number
+            height .32rem
+            line-height .32rem
+            padding 0 .4rem
+            margin-top .14rem
+            border-radius .2rem
+            background rgba(0,0,0,.8)
+            font-size .24rem
+            .banner-icon
+            // 字体覆盖
+                font-size .24rem
+</style>
